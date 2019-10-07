9bd3afbc794db3a2382c26aa68b118476780c67d
Ahmad Guenoun <amguenoun@gmail.com>
2019-10-07 15:30:10 -0400

    initialized countrylist

--- countries/src/main/java/com/lambda/countries/CountriesApplication.java
+++ countries/src/main/java/com/lambda/countries/CountriesApplication.java
@@ -5,8 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CountriesApplication {
+	static CountryList list;
 
 	public static void main(String[] args) {
+		list = new CountryList();
 		SpringApplication.run(CountriesApplication.class, args);
 	}
 
