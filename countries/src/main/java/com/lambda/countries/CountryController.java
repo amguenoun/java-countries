f64a8f5c26cc8018d4f3ea2f6a71a172ffe70ca1
Ahmad Guenoun <amguenoun@gmail.com>
2019-10-07 15:45:41 -0400

    finished first endpoint to return all countries alphabetically

--- countries/src/main/java/com/lambda/countries/CountryController.java
+++ countries/src/main/java/com/lambda/countries/CountryController.java
@@ -1,10 +1,18 @@
 package com.lambda.countries;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/data")
 public class CountryController {
-
+	//localhost:2019/data/names/all - return the names of all the countries alphabetically
+	@GetMapping(value = "/names/all", produces = {"application/json"})
+	public ResponseEntity<?> getAllCountries(){
+		CountriesApplication.list.countryList.sort((e1,e2) -> e1.getName().compareToIgnoreCase(e2.getName()));
+		return new ResponseEntity<>(CountriesApplication.list.countryList, HttpStatus.OK);
+	}
 }
